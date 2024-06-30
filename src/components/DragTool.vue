3113374d2c86a79fa725c9999ba3d06a706fb8d8
zeiaibengkui <2243925461@qq.com>
2024-06-30 19:20:42 +0800

    new file:   src/components/DragTool.vue 	modified:   src/components/SearchBar.vue

---
+++ src/components/DragTool.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class='drag' onmouseover="$('.drag').parent().draggable({ scroll: false, handle: '.drag' });">: : : :</div>
+</template>
+
+<script>
+
+
+
+</script>
+
+<style scoped lang='scss'>
+.drag {
+    position: absolute;
+    top: 0;
+    left: 50%;
+    margin-left: -8px;
+    cursor: grab;
+    font-size: 8px;
+    text-shadow: var(--bs-body-bg) 1px 1px;
+    display: flex;
+    justify-content: center;
+    width: 16px;
+}
+</style>
