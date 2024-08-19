c52b8f9eee58b4a8f36bef8cbaaac673156a071d
zeiaibengkui <2243925461@qq.com>
2024-08-19 14:17:59 +0800

    new file:   src/components/SearchSuggest.vue 	modified:   .prettierrc.json 	modified:   index.html 	modified:   src/App.vue 	modified:   src/assets/main.scss 	modified:   src/components/BootstrapTheme.vue 	modified:   src/components/DragTool.vue 	modified:   src/components/LoaclForageSettings.vue 	modified:   src/components/OtherSettings.ts 	modified:   src/components/SearchBar.vue 	modified:   src/components/SearchSuggest.vue 	modified:   src/components/WallPaper.vue 	modified:   src/components/contextMenu.vue 	modified:   src/main.ts

---
+++ src/components/SearchSuggest.vue
@@ -0,0 +1,36 @@
+<template>
+    <ul id='suggestions' class='container-fluid'>
+        <!-- <iframe
+        :src="`http://localhost:8012/proxy/AS/Suggestions?pt=page.home&mkt=zh-cn&qry=${text}&cp=1&msbqf=false&cvid=1`"
+        id="suggestionsFr"
+    >
+    </iframe> -->
+        <li v-for='item in suggestionsList' :key='item.text'>{{ item.text }}</li>
+    </ul>
+</template>
+<script>
+export default {
+    name: 'SearchSuggest'
+}
+</script>
+<style scoped lang='scss'>
+
+#suggestions {
+    //position: absolute;
+    //top: 19px;
+    //padding: 19px 0 0 0;
+    //background: var(--bs-body-bg);
+    margin-top: 0.5em;
+    transition: height 0.3s;
+    height: auto;
+
+    &:not(:has(li:first-child)) {
+        display: none;
+    }
+
+    li {
+        text-decoration: none;
+        list-style: none;
+    }
+}
+</style>
